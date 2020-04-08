fd6d46dca4bbc27baf250797bb3b9232eb544c9a
sdercolin <sder.colin@gmail.com>
2020-04-09 00:04:14 +0900

    Implement a value holder widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Desktop Game Helper (devcode)',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    final String title = 'Desktop Game Helper (devcode)';
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.requestFocus(new FocusNode());
+        }
+      },
+      child: MaterialApp(
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: title),
       ),
-      home: MyHomePage(title: 'Home Page'),
     );
   }
 }
@@ -29,12 +38,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static final int _counterDefault = 0;
+  int _counter = _counterDefault;
+  FocusNode _focus = new FocusNode();
+  bool _isFocus = false;
+
+  TextEditingController _textEditingController;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      _refreshCounterDisplay();
+      _focus.unfocus();
     });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _refreshCounterDisplay();
+      _focus.unfocus();
+    });
+  }
+
+  void _refreshCounterDisplay() {
+    _textEditingController.text = _counter.toString();
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      _isFocus = _focus.hasFocus;
+      if (!_isFocus) {
+        if (_textEditingController.text.isEmpty) {
+          _counter = _counterDefault;
+        } else {
+          int parsedValue = int.tryParse(_textEditingController.text);
+          if (parsedValue != null) _counter = parsedValue;
+        }
+        _refreshCounterDisplay();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController(text: _counter.toString());
+    _focus.addListener(_onFocusChange);
   }
 
   @override
@@ -47,21 +97,38 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            IconButton(
+              icon: Icon(Icons.arrow_drop_up),
+              onPressed: _incrementCounter,
+              iconSize: 50,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextField(
+                style: Theme.of(context).textTheme.headline3,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                controller: _textEditingController,
+                focusNode: _focus,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: _counterDefault.toString()),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_drop_down),
+              onPressed: _decrementCounter,
+              iconSize: 50,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 }
